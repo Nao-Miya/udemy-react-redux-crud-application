c51d971b19933a6265bbeaf311b08cd5e854a69a
Nao-Miya <33458065+Nao-Miya@users.noreply.github.com>
2020-07-07 13:00:45 +0900

    App.jsの修正

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+const App = () => (<Counter></Counter>)
 
-function App() {
-  return <div><h1 className="App-title">Hello, world!</h1></div>;
+class Counter extends Component{
+  constructor(props) {
+    super(props)
+    this.state ={count:0}
+  }
+
+  handlePlusButton = ()=>{
+    this.setState({count:this.state.count+1})
+  }
+
+  handleMinusButton = ()=>{
+    this.setState({count:this.state.count-1})
+  }
+
+  render(){
+    //console.log("render")
+    return(
+        <React.Fragment>
+          <div>count: {this.state.count }</div>
+          <button onClick={this.handlePlusButton}>+1</button>
+          <button onClick={this.handleMinusButton}>-1</button>
+        </React.Fragment>
+    )
+  }
 }
-
 export default App;
+
+
