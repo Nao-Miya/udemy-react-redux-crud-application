a75a90c3c7250b9dd09b7529806d986fcdd62494
Nao-Miya <33458065+Nao-Miya@users.noreply.github.com>
2020-07-09 19:02:20 +0900

    implement event new component

---
+++ src/components/events_new.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+//import {postEvent} from '../actions'
+
+
+class EventsNew extends Component{
+  render(){
+    //const props = this.props
+    return (
+      <React.Fragment>
+          <div>foo</div>
+      </React.Fragment>
+    )
+  }
+}
+
+
+//const mapDispatchToProps =({postEvents})
+
+export default connect(null, null)(EventsNew)
+
+
